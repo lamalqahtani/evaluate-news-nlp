7068ccb4721e8d39ac8ae3a85a7b6f65baa9b629
Lamyaa Q <lamiaa@Lamyaas-MacBook-Pro.local>
2022-09-01 12:08:29 +0300

    api creditials added, html file updated to let the for placeholder refere to inserting a url not a name

--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,11 @@
-var path = require('path')
+const path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
-
 const app = express()
+
+//API Part
+const baseURL = "https://api.meaningcloud.com/sentiment-2.1"
+const API_KEY = "95c2f34c0fa48d78f8c5923b8f970fd6"
 
 app.use(express.static('dist'))
 
